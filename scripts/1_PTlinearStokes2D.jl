debb91c80d3f6ea0f1162a1b1ae05c7ccf7b49c7
Ciril Humbel <ciril.humbel@gmail.com>
2024-09-27 15:07:39 +0200

    add residuals and check them as termination criterion

--- scripts/1_PTlinearStokes2D.jl
+++ scripts/1_PTlinearStokes2D.jl
@@ -1,15 +1,16 @@
 using CairoMakie
+using LinearAlgebra
 
-function update_P_τ!(Pt, τ, V, η, dx, dy, κΔθ)
+function update_P_τ!(Pt, τ, divV, V, η, dx, dy, κΔθ)
     nx, ny = size(Pt)
     for j = 1:ny
         for i = 1:nx
             dVx = (V.x[i+1, j] - V.x[i, j]) / dx
             dVy = (V.y[i, j+1] - V.y[i, j]) / dy
-            divV = dVx + dVy
-            Pt[i, j]  -= κΔθ[i, j] * divV
-            τ.xx[i, j] = 2 * η[i, j] * (dVx - 1/3 * divV)
-            τ.yy[i, j] = 2 * η[i, j] * (dVy - 1/3 * divV)
+            divV[i, j] = dVx + dVy
+            Pt[i, j]  -= κΔθ[i, j] * divV[i, j]
+            τ.xx[i, j] = 2 * η[i, j] * (dVx - 1/3 * divV[i, j])
+            τ.yy[i, j] = 2 * η[i, j] * (dVy - 1/3 * divV[i, j])
             if i < nx && j < ny
                 # average η to cell corner
                 η_c = 0.25 * (η[i, j] + η[i+1, j] + η[i, j+1] + η[i+1, j+1])
@@ -22,23 +23,25 @@ function update_P_τ!(Pt, τ, V, η, dx, dy, κΔθ)
 end
 
 
-function update_dV!(dV, Pt, τ, ρg, dx, dy, damp)
+function update_dV!(dV, R, Pt, τ, ρg, dx, dy, damp)
     nx, ny = size(Pt)
     # update velocity update in x
     for j = 1:ny-2
         for i = 1:nx-1
-            dV.x[i, j] = damp.x * dV.x[i, j] + ((τ.xx[i+1, j+1] - τ.xx[i, j+1]) / dx
-                                              + (τ.xy[i, j+1] - τ.xy[i, j]) / dy
-                                              - (Pt[i+1, j+1] - Pt[i, j+1]) / dx)
+            R.x[i, j]  = ((τ.xx[i+1, j+1] - τ.xx[i, j+1]) / dx
+                        + (τ.xy[i, j+1] - τ.xy[i, j]) / dy
+                        - (Pt[i+1, j+1] - Pt[i, j+1]) / dx)
+            dV.x[i, j] = damp.x * dV.x[i, j] + R.x[i, j]
         end
     end
 
     for j = 1:ny-1
         for i = 1:nx-2
-            dV.y[i, j] = damp.y * dV.y[i, j] + ((τ.yy[i+1, j+1] - τ.yy[i+1, j]) / dy
-                                              + (τ.xy[i+1, j] - τ.xy[i, j]) / dx
-                                              - (Pt[i+1, j+1] - Pt[i+1, j]) / dy
-                                              + (ρg[i+1, j+1] + ρg[i+1, j]) * 0.5)
+            R.y[i, j]  = ((τ.yy[i+1, j+1] - τ.yy[i+1, j]) / dy
+                        + (τ.xy[i+1, j] - τ.xy[i, j]) / dx
+                        - (Pt[i+1, j+1] - Pt[i+1, j]) / dy
+                        + (ρg[i+1, j+1] + ρg[i+1, j]) * 0.5)
+            dV.y[i, j] = damp.y * dV.y[i, j] + R.y[i, j]
         end
     end
 end
@@ -79,26 +82,28 @@ end
 function linearStokes2D()
     Lx = Ly = 10.
     R_in  = 1.
-    η_in  = 0.1
+    η_in  = 0.01
     η_out = 1.
     ρg_in = 1.
 
     nx = ny = 127
     max_iter = 10000
     max_err  = 1e-6
-    ncheck = 1000
+    ncheck = 100
 
     dx, dy = Lx / nx, Ly / ny
     xs = LinRange(-0.5Lx + 0.5dx, 0.5Lx - 0.5dx, nx)
     ys = LinRange(-0.5Ly + 0.5dy, 0.5Ly - 0.5dy, ny)
 
     # field initialisation
-    η  = [x^2 + y^2 < R_in^2 ? η_in : η_out for x=xs, y=ys]
-    ρg = [x^2 + y^2 < R_in^2 ? ρg_in : 0. for x=xs, y=ys]
-    Pt = zeros(nx, ny)
-    V  = (x=zeros(nx+1, ny), y=zeros(nx, ny+1))
-    dV = (x=zeros(nx-1, ny-2), y=zeros(nx-2, ny-1))
-    τ  = (xx=zeros(nx, ny), xy=zeros(nx-1, ny-1), yy=zeros(nx, ny))
+    η    = [x^2 + y^2 < R_in^2 ? η_in : η_out for x=xs, y=ys]
+    ρg   = [x^2 + y^2 < R_in^2 ? ρg_in : 0. for x=xs, y=ys]
+    Pt   = zeros(nx, ny)
+    divV = zeros(nx, ny)
+    V    = (x=zeros(nx+1, ny), y=zeros(nx, ny+1))
+    dV   = (x=zeros(nx-1, ny-2), y=zeros(nx-2, ny-1))
+    R    = (x=zeros(nx-1, ny-2), y=zeros(nx-2, ny-1))
+    τ    = (xx=zeros(nx, ny), xy=zeros(nx-1, ny-1), yy=zeros(nx, ny))
     κΔθ   = zeros(nx, ny)
     Δθ_ρx = zeros(nx-1, ny-2)
     Δθ_ρy = zeros(nx-2, ny-1)
@@ -112,45 +117,41 @@ function linearStokes2D()
     # visualisation
     fig = Figure()
     axs = (Pt=Axis(fig[1,1][1,1], aspect=1, xlabel="x", ylabel="y", title="Pressure"),
-           ρg=Axis(fig[1,2][1,1], aspect=1, xlabel="x", ylabel="y", title="Body Force"),
-           Vx=Axis(fig[2,1][1,1], aspect=1, xlabel="x", ylabel="y", title="Horizontal Velocity"),
-           Vy=Axis(fig[2,2][1,1], aspect=1, xlabel="x", ylabel="y", title="Vertical Velocity"))
+           η=Axis(fig[1,2][1,1], aspect=1, xlabel="x", ylabel="y", title="Viscosity"),
+           Vy=Axis(fig[2,1][1,1], aspect=1, xlabel="x", ylabel="y", title="Vertical Velocity"),
+           Ry=Axis(fig[2,2][1,1], aspect=1, xlabel="x", ylabel="y", title="Vertical Velocity Residual"))
     plt = (Pt=image!(axs.Pt, (xs[1], xs[end]), (ys[1], ys[end]), Pt, colormap=:inferno),
-           ρg=image!(axs.ρg, (xs[1], xs[end]), (ys[1], ys[end]), ρg, colormap=:inferno),
-           Vx=image!(axs.Vx, (xs[1], xs[end]), (ys[1]-dy/2, ys[end]+dy/2), V.x, colormap=:inferno),
-           Vy=image!(axs.Vy, (xs[1], xs[end]), (ys[1]-dy/2, ys[end]+dy/2), V.y, colormap=:inferno))
+           η=image!(axs.η, (xs[1], xs[end]), (ys[1], ys[end]), η, colormap=:inferno),
+           Vy=image!(axs.Vy, (xs[1], xs[end]), (ys[1]-dy/2, ys[end]+dy/2), V.y, colormap=:inferno),
+           Ry=image!(axs.Ry, (xs[1]+dx, xs[end]-dx), (ys[1]+dy/2, ys[end]-dy/2), R.y, colormap=:inferno))
     Colorbar(fig[1, 1][1, 2], plt.Pt)
-    Colorbar(fig[1, 2][1, 2], plt.ρg)
-    Colorbar(fig[2, 1][1, 2], plt.Vx)
-    Colorbar(fig[2, 2][1, 2], plt.Vy)
+    Colorbar(fig[1, 2][1, 2], plt.η)
+    Colorbar(fig[2, 1][1, 2], plt.Vy)
+    Colorbar(fig[2, 2][1, 2], plt.Ry)
 
     # loop
-    err_Pt = 2 * max_err
+    err = 2 * max_err
     it = 1
-    while it <= max_iter && err_Pt > max_err
-        update_P_τ!(Pt, τ, V, η, dx, dy, κΔθ)
-        update_dV!(dV, Pt, τ, ρg, dx, dy, damp)
+    while it <= max_iter && err > max_err
+        update_P_τ!(Pt, τ, divV, V, η, dx, dy, κΔθ)
+        update_dV!(dV, R, Pt, τ, ρg, dx, dy, damp)
         update_V!(V, dV, Δθ_ρx, Δθ_ρy)
         bc_y!(V.x)
         bc_x!(V.y)
         if it % ncheck == 0
-            err_Pt = 0
-            for j = 1:ny
-                for i = 1:nx
-                    err_Pt += (V.x[i+1, j] - V.x[i, j]) / dx + (V.y[i, j+1] - V.y[i, j]) / dy
-                end
-            end
-            err_Pt /= nx*ny  # mean error
-            println("iteration ", it, ": max(div(V)) = ", err_Pt)
-            plt.Pt[3] = Pt
-            plt.Vx[3] = V.x
-            plt.Vy[3] = V.y
-            display(fig)
+            err = max(norm(divV, 1)/length(divV), norm(R.x, 1)/length(R.x), norm(R.y, 1)/length(R.y))
+            
+            println("iteration ", it, ": err = ", err)
+            
         end
         it += 1
     end
 
-
+    println("total number of iterations: ", it)
+    plt.Pt[3] = Pt
+    plt.Vy[3] = V.y
+    plt.Ry[3] = log10.(abs.(R.y))
+    display(fig)
 
     return nothing
 
