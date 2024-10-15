7e95aa175294d0bb0243f0288017d52696237234
Ciril Humbel <ciril.humbel@gmail.com>
2024-10-15 12:06:11 +0200

    experiment with FD-like code and autodiff

--- scripts/2b_autodiff.jl
+++ scripts/2b_autodiff.jl
@@ -3,13 +3,18 @@ using LinearAlgebra
 
 
 function f(s, u, a, b, c)
-    @. s.x  = a * u.x + b * u.y + c
-    @. s.y  = a * u.y - b * u.x + 5c
+    for i = axes(u.x, 1)[1:end-1]
+        s.x[i] = a * (u.x[i+1] - u.x[i]) + b * (u.y[i+1] - u.y[i]) + c
+        s.y[i] = a * (u.y[i+1] - u.y[i]) + b * (u.x[i+1] - u.x[i]) + 5c
+    end
 end
 
-u = (x=ones(3,3), y=ones(3,3))
-p = (x=zeros(3,3), y=zeros(3,3))
-p.x[1,1] = 1
-s = (x=zeros(3,3), y=zeros(3,3))
-t = (x=zeros(3,3), y=zeros(3,3))
-autodiff(Forward, f, Duplicated(s, t), Duplicated(u, p), Const(3), Const(2), Const(10000))
+u = (x=rand(10), y=rand(10))
+p = (x=ones(10), y=ones(10))
+p.x[1] = 0; p.x[end] = 0
+p.y[1] = 0; p.y[end] = 0
+s = (x=zeros(9), y=zeros(9))
+t = (x=zeros(9), y=zeros(9))
+# autodiff(Forward, f, Duplicated(s, t), Duplicated(u, p), Const(3), Const(2), Const(10000))
+
+f(t, p, 3, 2, 0)
