b5073fb6f4ffa13c37f277dff27758048decf827
Ciril Humbel <ciril.humbel@gmail.com>
2024-10-15 12:05:33 +0200

    correct sign errors; improved plots

--- scripts/2_augmentedLagrangian2D.jl
+++ scripts/2_augmentedLagrangian2D.jl
@@ -3,15 +3,15 @@ using LinearAlgebra
 using Enzyme
 
 
-function compute_P!(P, P_old, V, dx, dy, γ)
+function compute_P!(P, divV, V, dx, dy, γ)
     nx, ny = size(P)
     # compute pressure
     for j = 1:ny
         for i = 1:nx
-            dVx  = (V.x[i+1, j] - V.x[i, j]) / dx
-            dVy  = (V.y[i, j+1] - V.y[i, j]) / dy
-            divV = dVx + dVy
-            P[i, j]  = P_old[i, j] - γ * divV
+            dVx = (V.x[i+1, j] - V.x[i, j]) / dx
+            dVy = (V.y[i, j+1] - V.y[i, j]) / dy
+            divV[i, j] = dVx + dVy
+            P[i, j] -= γ * divV[i, j]
         end
     end
     return nothing
@@ -26,8 +26,8 @@ function compute_R!(R, P, V, ρg, η, dx, dy, γ)
     for j = 1:ny-2
         for i = 1:nx-1
             # stress at horizontally adjacent cell centers
-            τxx_r = (2 * η[i+1, j+1] - γ) * (V.x[i+2, j+1] - V.x[i+1, j+1]) / dx - γ * (V.y[i+1, j+2] - V.y[i+1, j+1]) / dy
-            τxx_l = (2 * η[i  , j+1] - γ) * (V.x[i+1, j+1] - V.x[i  , j+1]) / dx - γ * (V.y[i  , j+2] - V.y[i  , j+1]) / dy
+            τxx_r = (2 * η[i+1, j+1] + γ) * (V.x[i+2, j+1] - V.x[i+1, j+1]) / dx + γ * (V.y[i+1, j+2] - V.y[i+1, j+1]) / dy
+            τxx_l = (2 * η[i  , j+1] + γ) * (V.x[i+1, j+1] - V.x[i  , j+1]) / dx + γ * (V.y[i  , j+2] - V.y[i  , j+1]) / dy
             # viscosity at vertically adjacent cell corners
             η_t   = 0.25 * (η[i, j+1] + η[i+1, j+1] + η[i, j+2] + η[i+1, j+2])
             η_b   = 0.25 * (η[i, j  ] + η[i+1, j  ] + η[i, j+1] + η[i+1, j+1])
@@ -37,17 +37,17 @@ function compute_R!(R, P, V, ρg, η, dx, dy, γ)
             τxy_b = η_b * ((V.x[i+1, j+1] - V.x[i+1, j  ]) / dy
                          + (V.y[i+1, j+1] - V.y[i  , j+1]) / dx)
             # residual in x direction on the interface
-            R.x[i, j]  = ((τxx_r - τxx_l) / dx
-                        + (τxy_t - τxy_b) / dy
-                        - (P[i+1, j+1] - P[i, j+1]) / dx)
+            R.x[i, j]  = (- (τxx_r - τxx_l) / dx
+                          - (τxy_t - τxy_b) / dy
+                          + (P[i+1, j+1] - P[i, j+1]) / dx)
         end
     end
 
     # residual in y direction
     for j = 1:ny-1
         for i = 1:nx-2
-            τyy_t = (2 * η[i+1, j+1] - γ) * (V.y[i+1, j+2] - V.y[i+1, j+1]) / dy - γ * (V.x[i+2, j+1] - V.x[i+1, j+1]) / dx
-            τyy_b = (2 * η[i+1, j  ] - γ) * (V.y[i+1, j+1] - V.y[i+1, j  ]) / dy - γ * (V.x[i+2, j  ] - V.x[i+1, j  ]) / dx
+            τyy_t = (2 * η[i+1, j+1] + γ) * (V.y[i+1, j+2] - V.y[i+1, j+1]) / dy + γ * (V.x[i+2, j+1] - V.x[i+1, j+1]) / dx
+            τyy_b = (2 * η[i+1, j  ] + γ) * (V.y[i+1, j+1] - V.y[i+1, j  ]) / dy + γ * (V.x[i+2, j  ] - V.x[i+1, j  ]) / dx
 
             η_r   = 0.25 * (η[i+1, j  ] + η[i+2, j  ] + η[i+1, j+1] + η[i+2, j+1])
             η_l   = 0.25 * (η[i  , j  ] + η[i+1, j  ] + η[i  , j+1] + η[i+1, j+1])
@@ -57,19 +57,19 @@ function compute_R!(R, P, V, ρg, η, dx, dy, γ)
             τxy_l = η_l * ((V.x[i+1, j+1] - V.x[i+1, j  ]) / dy
                          + (V.y[i+1, j+1] - V.y[i  , j+1]) / dx)
             
-            R.y[i, j]  = ((τyy_t - τyy_b) / dy
-                        + (τxy_r - τxy_l) / dx
-                        - (P[i+1, j+1] - P[i+1, j]) / dy
-                        + (ρg[i+1, j+1] + ρg[i+1, j]) * 0.5)
+            R.y[i, j]  = ( - (τyy_t - τyy_b) / dy
+                           - (τxy_r - τxy_l) / dx
+                           + (P[i+1, j+1] - P[i+1, j]) / dy
+                           - (ρg[i+1, j+1] + ρg[i+1, j]) * 0.5)
         end
     end
     return nothing
 end
 
 
-function compute_β_normR(normR², R)
-    normR²_new = dot(R.x, R.x) + dot(R.y, R.y)
-    return normR²_new / normR², normR²_new
+function compute_β_rMr(rMr, R, Minv)
+    rMr_new = dot(R.x, Minv.x .* R.x) + dot(R.y, Minv.y .* R.y)
+    return rMr_new / rMr, rMr_new
 end
 
 
@@ -89,25 +89,24 @@ function update_D!(D, R, Minv, β)
 end
 
 
-function compute_α(R, Ad, P, V, D, ρg, η, normR², dx, dy, γ)
+function compute_α(R, Ad, P, V, D, ρg, η, rMr, dx, dy, γ)
     # compute Jacobian-vector product Jac(R) * D using Enzyme
     # result is stored in Ad
     autodiff(Forward, compute_R!, DuplicatedNoNeed(R, Ad), Const(P), Duplicated(V, D), Const(ρg), Const(η), Const(dx), Const(dy), Const(γ))
-    # compute α = -dot(R, R) / dot(D, A*D)
-    # negative sign because matrix A produces the negative divergenve of stress 
-    return  -normR² / (dot(D.x[2:end-1, 2:end-1], Ad.x) + dot(D.y[2:end-1, 2:end-1], Ad.y))
+    # compute α = -dot(R, M*R) / dot(D, A*D)
+    return  rMr / (dot(D.x[2:end-1, 2:end-1], Ad.x) + dot(D.y[2:end-1, 2:end-1], Ad.y))
 end
 
 
-function update_V!(V, D, Minv, α)
+function update_V!(V, D, α)
     for j = 2:size(V.x, 2)-1
         for i = 2:size(V.x, 1)-1
-            V.x[i, j] += α * Minv.x[i-1, j-1] * D.x[i, j]
+            V.x[i, j] -= α * D.x[i, j]
         end
     end
     for j = 2:size(V.y, 2)-1
         for i = 2:size(V.y, 1)-1
-            V.y[i, j] += α * Minv.y[i-1, j-1] * D.y[i, j]
+            V.y[i, j] -= α * D.y[i, j]
         end
     end
     return nothing
@@ -131,7 +130,7 @@ function neumann_bc_y!(f)
 end
 
 
-function linearStokes2D(η_ratio=0.1; max_iter=100000, ncheck=2000, max_err=1e-6, n=127)
+function linearStokes2D(η_ratio=0.1; niter_in=1000, niter_out=1000, ncheck=2000, max_err=1e-6, n=127)
     Lx = Ly = 10.
     R_in  = 1.
     η_out = 1.
@@ -148,7 +147,7 @@ function linearStokes2D(η_ratio=0.1; max_iter=100000, ncheck=2000, max_err=1e-6
     η      = [x^2 + y^2 < R_in^2 ? η_in  : η_out for x=xs, y=ys]
     ρg     = [x^2 + y^2 < R_in^2 ? ρg_in : 0.    for x=xs, y=ys]
     P      = zeros(nx, ny)
-    P_old  = zeros(nx, ny)
+    divV   = zeros(nx, ny)
     V      = (x =zeros(nx+1, ny  ), y =zeros(nx  , ny+1))
     D      = (x =zeros(nx+1, ny  ), y =zeros(nx  , ny+1))  # search direction of CG, outer cells are zero
     R      = (x =zeros(nx-1, ny-2), y =zeros(nx-2, ny-1))  # Residuals of velocity PDE
@@ -165,12 +164,13 @@ function linearStokes2D(η_ratio=0.1; max_iter=100000, ncheck=2000, max_err=1e-6
     γ = inv(maximum(η))
     
     # visualisation
-    errs = []
+    errs_in = []
+    errs_out = []
 
     err_out = 2 * max_err
     it_out = 1
     # outer loop, Powell Hestenes
-    while it_out <= 1 && err_out > max_err
+    while it_out <= niter_out && err_out > max_err
         err_in = 2 * max_err
         it_in = 1
         # inner loop, Conjugate Gradient
@@ -178,57 +178,64 @@ function linearStokes2D(η_ratio=0.1; max_iter=100000, ncheck=2000, max_err=1e-6
         compute_R!(R, P, V, ρg, η, dx, dy, γ)
         D.x[2:end-1, 2:end-1] .= Minv.x .* R.x
         D.y[2:end-1, 2:end-1] .= Minv.y .* R.y
-        normR² = dot(R.x, R.x) + dot(R.y, R.y)
-        while it_in <= max_iter && err_in > max_err
-            α = compute_α(R, Ad, P, V, D, ρg, η, normR², dx, dy, γ)
-            update_V!(V, D, Minv, α)
+        rMr = dot(R.x, Minv.x .* R.x) + dot(R.y, Minv.y .* R.y)
+        while it_in <= niter_in && err_in > max_err
+            α = compute_α(R, Ad, P, V, D, ρg, η, rMr, dx, dy, γ)
+            update_V!(V, D, α)
             neumann_bc_y!(V.x)
             neumann_bc_x!(V.y)
             compute_R!(R, P, V, ρg, η, dx, dy, γ)
-            β, normR² = compute_β_normR(normR², R)
+            β, rMr = compute_β_rMr(rMr, R, Minv)
             update_D!(D, R, Minv, β)
-            err_in = sqrt(normR²) / (length(R.x) + length(R.y))
+
             if it_in % ncheck == 0
-                push!(errs, err_in)
-                println(it_in ," inner iterations: error = ", err_in)
+                err_in = (norm(R.x, 1) + norm(R.y, 1)) / (length(R.x) + length(R.y))
+                push!(errs_in, err_in)
+                println("\t", it_in ," inner iterations: error = ", err_in)
             end
             it_in += 1
         end
-        compute_P!(P, P_old, V, dx, dy, γ)
-        # err_out = sqrt(norm∇V²) / length(P)
-        # push!(errs, err_out)
-        # println(it_out , " outer iterations: performed ", it_in ," inner iterations, error = ", err_out)
+        compute_P!(P, divV, V, dx, dy, γ)
+        err_out = norm(divV, 1) / length(divV)
+        push!(errs_out, err_out)
+        println(it_out , " outer iterations: error = ", err_out)
         it_out += 1
     end
 
     # println("outer iteration stopped at $it_out with error $err_out")
 
-    return P, V, R, errs, xs, ys
+    return P, V, R, errs_in, errs_out, xs, ys
 end
 
 
-function create_output_plot(P, V, R, errs, xs, ys, ncheck, η_ratio; savefig=false)
+function create_output_plot(P, V, R, errs_in, errs_out, xs, ys; ncheck, ninner, η_ratio, savefig=false)
     dy = ys[2] - ys[1]
-    fig = Figure()
+    fig = Figure(size=(800, 600))
     axs = (P=Axis(fig[1,1][1,1], aspect=1, xlabel="x", ylabel="y", title="Pressure"),
-        err=Axis(fig[1,2][1,1], xlabel="Iterations", title="Residual (log)"),
+        err=Axis(fig[1,2][1,1], xlabel="Iterations", title="Mean Absolute Residual (log)"),
         Vy=Axis(fig[2,1][1,1], aspect=1, xlabel="x", ylabel="y", title="Vertical Velocity"),
         Ry=Axis(fig[2,2][1,1], aspect=1, xlabel="x", ylabel="y", title="Vertical Velocity Residual (log)"))
+    # compute location of outer iteration errors
+    iters_out = [ninner * i for i=1:length(errs_out)]
+    iters_out[end] = ncheck * length(errs_in)
+    scatter!(axs.err, iters_out, log10.(errs_out), color=:blue, label="Pressure")
     plt = (P=image!(axs.P, (xs[1], xs[end]), (ys[1], ys[end]), P, colormap=:inferno),
-           err=lines!(axs.err, ncheck .* 1:length(errs), log10.(errs)),
+           err=lines!(axs.err, ncheck .* (1:length(errs_in)), log10.(errs_in), color=:red, label="Velocity"),
            Vy=image!(axs.Vy, (xs[1], xs[end]), (ys[1]-dy/2, ys[end]+dy/2), V.y, colormap=:inferno),
            Ry=image!(axs.Ry, (xs[2], xs[end-1]), (ys[1]+dy/2, ys[end]-dy/2), log10.(abs.(R.y)), colormap=:inferno))
     Colorbar(fig[1, 1][1, 2], plt.P)
     Colorbar(fig[2, 1][1, 2], plt.Vy)
     Colorbar(fig[2, 2][1, 2], plt.Ry)
+    axislegend(axs.err, position=:rt)
 
     if savefig
-        save("1_output_$(η_ratio)_$(n).png", fig)
+        save("2_output_$(η_ratio)_$(ninner).png", fig)
     else
         display(fig)
     end
     return nothing
 end
+
 
 
 function create_convergence_plot(errs, ncheck, η_ratio; savefig=false)
@@ -244,6 +251,11 @@ function create_convergence_plot(errs, ncheck, η_ratio; savefig=false)
     
 end
 
-outfields = linearStokes2D(0.1; max_iter=500, ncheck=50)
+eta = 0.1
+ninner=500
+nouter=100
+ncheck=50
 
-create_output_plot(outfields..., 1, 0.1)
+outfields = linearStokes2D(eta; niter_in=ninner, niter_out=nouter, ncheck=ncheck)
+
+create_output_plot(outfields...; ncheck=ncheck, ninner=ninner, η_ratio=eta, savefig=true)
