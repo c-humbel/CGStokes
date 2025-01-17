e854f4df35893ced382980223a328acb7deef946
Ciril Humbel <ciril.humbel@gmail.com>
2025-01-17 13:33:39 +0100

    implement addition and subtraction of named tuples

--- src/tuple_manip.jl
+++ src/tuple_manip.jl
@@ -15,27 +15,44 @@ end
 
 
 function tplDot(x::NamedTuple, y::NamedTuple, a::Real=1.)
-    return sum(dot.(values(x), a .* values(y)))
+    return a * sum(dot.(values(x), values(y)))
 end
 
 
 function tplSet!(dest::NamedTuple, src::NamedTuple, a::NamedTuple)
     for k = keys(dest)
-        copyto!(dest[k], a[k] .* src[k])
+        copyto!(dest[k], src[k])
+        dest[k] .*= a[k]
     end
     return nothing
 end
 
 
 function tplSet!(dest::NamedTuple, src::NamedTuple, a::Real=1.)
-    copyto!.(values(dest), a .* values(src))
+    copyto!.(values(dest), values(src))
+    tplScale!(dest, a)
     return nothing
 end
 
 
 function tplScale!(x::NamedTuple, a::Real)
     for k = keys(x)
-        x[k] .= a .* x[k]
+        x[k] .*= a
+    end
+    return nothing
+end
+
+
+function tplAdd!(this::NamedTuple, other::NamedTuple)
+    for k = keys(this)
+        this[k] .+= other[k]
+    end
+    return nothing
+end
+
+function tplSub!(this::NamedTuple, other::NamedTuple)
+    for k = keys(this)
+        this[k] .-= other[k]
     end
     return nothing
 end
