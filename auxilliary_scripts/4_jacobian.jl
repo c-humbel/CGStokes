1c75547d618fa6631aefc485d504b67e8fad7813
chumbel <chumbel@ethz.ch>
2025-01-30 17:37:56 +0100

    separate file for shared functions

--- auxilliary_scripts/4_jacobian.jl
+++ auxilliary_scripts/4_jacobian.jl
@@ -1,176 +1,15 @@
 using Enzyme
 using Random
 
-function compute_R!(R, P, η, P₀, V, ρg, B, q, dx, dy, γ, background=eps())
-    nx, ny = size(P.c)    ### Dirichlet boundary conditions
-    # wall normal velocities are zero
-    for j = 1:ny
-        V.xc[1  , j] = 0.
-        V.xc[end, j] = 0.
-    end
-
-    for j = 1:ny+1
-        V.xv[1  , j] = 0.
-        V.xv[end, j] = 0.
-    end
-
-    for i = 1:nx
-        V.yc[i, 1  ] = 0.
-        V.yc[i, end] = 0.
-    end
-
-    for i = 1:nx+1
-        V.yv[i, 1  ] = 0.
-        V.yv[i, end] = 0.
-    end
-
-    ### pressure and viscosity update
-    for j = 1:ny
-        for i = 1:nx
-            dVxdx = (V.xc[i+1, j] - V.xc[i, j]) / dx 
-            dVydy = (V.yc[i, j+1] - V.yc[i, j]) / dy
-
-            P.c[i, j] = P₀.c[i, j] - γ * (dVxdx + dVydy)
-
-            dVxdy_dVydx = 0.5*((V.xv[i+1, j+1] - V.xv[i+1, j]) / dy + (V.yv[i+1, j+1] - V.yv[i, j+1]) / dx)
-            
-            η.c[i, j] = 0.5 * B * (0.5 * dVxdx^2 + 0.5 * dVydy^2 + dVxdy_dVydx^2 + background) ^ (0.5*q - 1)
-
-
-        end
-    end
-
-    for j = 1:ny+1
-        for i = 1:nx+1
-            dVxdx = (V.xv[i+1, j] - V.xv[i, j]) / dx
-            dVydy = (V.yv[i, j+1] - V.yv[i, j]) / dy
-
-            P.v[i, j] = P₀.v[i, j] - γ * (dVxdx + dVydy)
-
-            dVxdy = 1 < j < ny+1 ? 0.5 * (V.xc[i, j] - V.xc[i, j-1]) / dy : 0.  # gradient of wall parallel velocity is zero
-            dVydx = 1 < i < nx+1 ? 0.5 * (V.yc[i, j] - V.yc[i-1, j]) / dx : 0.  # gradient of wall parallel velocity is zero
-
-            η.v[i, j] = 0.5 * B * (0.5 * dVxdx^2 + 0.5 * dVydy^2 + (dVxdy + dVydx)^2 + background) ^ (0.5*q - 1)
-
-        end
-    end
-
-    ### residual in horizontal (x) direction
-    ## including Neumann BC on at top and bottom boundary
-    ## for velocities associated with cell centers (V.xc)
-    for j = 1:ny  # all values in y direction
-        for i = 2:nx  # inner values in x direction
-            # stress at horizontally adjacent cell centers
-            τxx_r = 2 * η.c[i  , j] * (V.xc[i+1, j] - V.xc[i  , j]) / dx
-            τxx_l = 2 * η.c[i-1, j] * (V.xc[i  , j] - V.xc[i-1, j]) / dx
-
-            # stress at vertically adjacent cell corners
-            if j > 1
-                τxy_b = η.v[i, j  ] * ((V.xc[i, j  ] - V.xc[i, j-1]) / dy + (V.yc[i, j  ] - V.yc[i-1, j  ]) / dx)
-            else
-                τxy_b = 0.  # zero stress at the bottom boundary
-            end
-
-            if j < ny
-                τxy_t = η.v[i, j+1] * ((V.xc[i, j+1] - V.xc[i, j  ]) / dy + (V.yc[i, j+1] - V.yc[i-1, j+1]) / dx)
-            else
-                τxy_t = 0.  # zero stress at the top boundary
-            end
-
-
-            # residual in x direction on the interface
-            R.xc[i, j]  = ( (τxx_r - τxx_l) / dx
-                          + (τxy_t - τxy_b) / dy
-                          - (P.c[i, j] - P.c[i-1, j]) / dx)
-        end
-    end
-    ## for velocities associated with cell corners (V.xv)
-    for j = 1:ny+1  # all values in y direction
-        for i = 2:nx+1  # inner values in x direction
-            τxx_r = 2 * η.v[i  , j] * (V.xv[i+1, j] - V.xv[i  , j]) / dx
-            τxx_l = 2 * η.v[i-1, j] * (V.xv[i  , j] - V.xv[i-1, j]) / dx
-
-            if j > 1
-                τxy_b = η.c[i-1, j-1] * ((V.xv[i, j  ] - V.xv[i, j-1]) / dy + (V.yv[i, j  ] - V.yv[i-1, j  ]) / dx)
-            else
-                τxy_b = 0.  # zero stress at the bottom boundary
-            end
-
-            if j < ny+1
-                τxy_t = η.c[i-1, j  ] * ((V.xv[i, j+1] - V.xv[i, j  ]) / dy + (V.yv[i, j+1] - V.yv[i-1, j+1]) / dx)
-            else
-                τxy_t = 0.  # zero stress at the top boundary
-            end
-
-            R.xv[i, j] = ( (τxx_r - τxx_l) / dx
-                         + (τxy_t - τxy_b) / dy
-                         - (P.v[i, j] - P.v[i-1, j]) / dx)
-        end
-    end
-
-    ### residual in vertical (y) direction
-    ### including Neumann BC at left and right boundary
-    ## for velocities associated with cell centers (V.yc)
-    for j = 2:ny  # inner values in y direction
-        for i = 1:nx  # all values in x direction
-            τyy_t = 2 * η.c[i, j  ] * (V.yc[i, j+1] - V.yc[i, j  ]) / dy
-            τyy_b = 2 * η.c[i, j-1] * (V.yc[i, j  ] - V.yc[i, j-1]) / dy
-
-            if i > 1
-                τxy_l = η.v[i  , j] * ((V.xc[i  , j] - V.xc[i  , j-1]) / dy + (V.yc[i  , j] - V.yc[i-1, j]) / dx)
-            else
-                τxy_l = 0.  # zero stress at the left boundary
-            end
-
-            if i < nx
-                τxy_r = η.v[i+1, j] * ((V.xc[i+1, j] - V.xc[i+1, j-1]) / dy + (V.yc[i+1, j] - V.yc[i  , j]) / dx)
-            else
-                τxy_r = 0.  # zero stress at the right boundary
-            end
-            
-            R.yc[i, j] = ( (τyy_t - τyy_b) / dy
-                         + (τxy_r - τxy_l) / dx
-                         - ( P.c[i, j] -  P.c[i, j-1]) / dy
-                         - (ρg.c[i, j] + ρg.c[i, j-1]) * 0.5)
-        end
-    end
-    ## for velocities associated with cell corners (V.yv)
-    for j=2:ny+1
-        for i=1:nx+1
-            τyy_t = 2 * η.v[i, j  ] * (V.yv[i, j+1] - V.yv[i, j  ]) / dy
-            τyy_b = 2 * η.v[i, j-1] * (V.yv[i, j  ] - V.yv[i, j-1]) / dy
-
-            if i > 1
-                τxy_l = η.c[i-1, j-1] * ((V.xv[i  , j] - V.xv[i  , j-1]) / dy + (V.yv[i  , j] - V.yv[i-1, j]) / dx)
-            else
-                τxy_l = 0.  # zero stress at the left boundary
-            end
-
-            if i < nx+1
-                τxy_r = η.c[i  , j-1] * ((V.xv[i+1, j] - V.xv[i+1, j-1]) / dy + (V.yv[i+1, j] - V.yv[i  , j]) / dx)
-            else
-                τxy_r = 0.  # zero stress at the right boundary
-            end
-
-            R.yv[i, j] = ( (τyy_t - τyy_b) / dy
-                         + (τxy_r - τxy_l) / dx
-                         - ( P.v[i, j] -  P.v[i, j-1]) / dy
-                         + (ρg.v[i, j] + ρg.v[i, j-1]) * 0.5)
-        end
-    end
-
-    # Residuals corresponding to cells affected by Dirichlet BC are left zero
-    return nothing
-end
-
+include("../scripts/4_nonlinear_augmLagrange/square_domain_free_slip.jl")
 
 function construct_jacobian_with_boundary(n=5, seed=1234)
     rng = Random.MersenneTwister(seed)
     nx, ny = n, n
     dx, dy = 1/nx, 1/ny
     γ      = 5.0
-    B      = 0.1
     q    = 1.33
+    B    = (c=ones(nx, ny), v=ones(nx+1, ny+1))
     P    = (c=zeros(nx, ny), v=zeros(nx+1, ny+1))
     ρg   = deepcopy(P)
     η    = deepcopy(P)
@@ -202,7 +41,8 @@ function construct_jacobian_with_boundary(n=5, seed=1234)
                 # set one entry in search vector to 1
                 d[i, j] = 1.0
                 # compute the jacobian column by multiplying it with a "basis vector"
-                autodiff(Forward, compute_R!, DuplicatedNoNeed(R, Q), Duplicated(P, P̄), Duplicated(η, η̄), Const(P₀), Duplicated(V, D), Const(ρg), Const(B), Const(q), Const(dx), Const(dy), Const(γ))
+                autodiff(Forward, compute_R!, DuplicatedNoNeed(R, Q), Duplicated(P, P̄), Duplicated(η, η̄), Const(P₀),
+                        Duplicated(V, D), Const(ρg), Const(B), Const(q), Const(eps()), Const(dx), Const(dy), Const(γ))
                 # store result in jacobian
                 J[1:E_xc, col]      .= reshape(Q.xc, N_xc)
                 J[E_xc+1:E_yc, col] .= reshape(Q.yc, N_yc)
@@ -227,8 +67,8 @@ function construct_jacobian(n=5; seed=1)
     nx, ny = n, n
     dx, dy = 1/nx, 1/ny
     γ      = 5.0
-    B      = 0.1
     q      = 1.33
+    B    = (c=ones(nx, ny), v=ones(nx+1, ny+1))
     P    = (c=zeros(nx, ny), v=zeros(nx+1, ny+1))
     ρg   = deepcopy(P)
     η    = deepcopy(P)
@@ -265,7 +105,8 @@ function construct_jacobian(n=5; seed=1)
                 # set one entry in search vector to 1
                 D[k][i, j] = 1.0
                 # compute the jacobian column by multiplying it with a "basis vector"
-                autodiff(Forward, compute_R!, DuplicatedNoNeed(R, Q), Duplicated(P, P̄), Duplicated(η, η̄), Const(P₀), Duplicated(V, D), Const(ρg), Const(B), Const(q), Const(dx), Const(dy), Const(γ))
+                autodiff(Forward, compute_R!, DuplicatedNoNeed(R, Q), Duplicated(P, P̄), Duplicated(η, η̄), Const(P₀),
+                        Duplicated(V, D), Const(ρg), Const(B), Const(q), Const(eps()), Const(dx), Const(dy), Const(γ))
                 # store result in jacobian
                 J[1:E_xc, col]      .= reshape(Q.xc[2:end-1, :], N_xc)
                 J[E_xc+1:E_yc, col] .= reshape(Q.yc[:, 2:end-1], N_yc)
--- scripts/4_nonlinear_augmLagrange/A_manyInclusions.jl
+++ scripts/4_nonlinear_augmLagrange/A_manyInclusions.jl
@@ -3,7 +3,7 @@ using ColorSchemes
 using Enzyme
 using Random
 
-include("../../src/tuple_manip.jl")
+include("square_domain_free_slip.jl")
 
 # copied from 2_augmentedLagrange/D_ManyInclusions_Egrid.jl
 function generate_inclusions(ninc, xs, ys, rng)
@@ -44,7 +44,7 @@ function generate_inclusions(ninc, xs, ys, rng)
 end
 
 # copied from 2_augmentedLagrange/D_ManyInclusions_Egrid.jl
-function initialise_η_ρ!(η, ρg, η_avg, ρg_avg, η_ratio, xc, yc, xv, yv, Lx, Ly; seed=1234, ninc=5)
+function initialise_B_ρ!(B, ρg, η_avg, ρg_avg, η_ratio, xc, yc, xv, yv, Lx, Ly; seed=1234, ninc=5)
     rng = MersenneTwister(seed)
 
     # generate radius and location inclusions
@@ -72,15 +72,15 @@ function initialise_η_ρ!(η, ρg, η_avg, ρg_avg, η_ratio, xc, yc, xv, yv, L
     Δρg   = ρg_avg * A_tot / A_inc
 
     # set viscosity and body force values
-    η.c  .= η_mat
-    η.v  .= η_mat
+    B.c  .= η_mat
+    B.v  .= η_mat
     ρg.c .= 0.
     ρg.v .= 0.
     for j = eachindex(yc)
         for i = eachindex(xc)
             for ((x, y), r, η_rel) ∈ zip(centers, radii, η_ratios)
                 if (xc[i] - x)^2 + (yc[j] - y)^2 <= r^2
-                    η.c[i, j]  = η_rel * η_mat
+                    B.c[i, j]  = η_rel * η_mat
                     ρg.c[i, j] = Δρg
                     break
                 end
@@ -92,7 +92,7 @@ function initialise_η_ρ!(η, ρg, η_avg, ρg_avg, η_ratio, xc, yc, xv, yv, L
         for i = eachindex(xv)
             for ((x, y), r, η_rel) ∈ zip(centers, radii, η_ratios)
                 if (xv[i] - x)^2 + (yv[j] - y)^2 <= r^2
-                    η.v[i, j]  = η_rel * η_mat
+                    B.v[i, j]  = η_rel * η_mat
                     ρg.v[i, j] = Δρg
                     break
                 end
@@ -104,337 +104,8 @@ function initialise_η_ρ!(η, ρg, η_avg, ρg_avg, η_ratio, xc, yc, xv, yv, L
     return nothing
 end
 
-function compute_divV!(divV, V, dx, dy)
-    nx, ny = size(divV.c)
-    for j = 1:ny
-        for i = 1:nx
-            dVx = (V.xc[i+1, j] - V.xc[i, j]) / dx
-            dVy = (V.yc[i, j+1] - V.yc[i, j]) / dy
-            divV.c[i, j] = dVx + dVy
-        end
-    end
 
-    for j = 1:ny+1
-        for i = 1:nx+1
-            dVx = (V.xv[i+1, j] - V.xv[i, j]) / dx
-            dVy = (V.yv[i, j+1] - V.yv[i, j]) / dy
-            divV.v[i, j] = dVx + dVy
-        end
-    end
-
-    return nothing
-end
-
-function compute_R!(R, P, η, P₀, V, ρg, B, q, ϵ̇_bg, dx, dy, γ)
-    nx, ny = size(P.c)
-
-    ### Dirichlet boundary conditions
-    # wall normal velocities are zero
-    for j = 1:ny
-        V.xc[1  , j] = 0.
-        V.xc[end, j] = 0.
-    end
-
-    for j = 1:ny+1
-        V.xv[1  , j] = 0.
-        V.xv[end, j] = 0.
-    end
-
-    for i = 1:nx
-        V.yc[i, 1  ] = 0.
-        V.yc[i, end] = 0.
-    end
-
-    for i = 1:nx+1
-        V.yv[i, 1  ] = 0.
-        V.yv[i, end] = 0.
-    end
-
-    ### pressure and viscosity update
-    for j = 1:ny
-        for i = 1:nx
-            dVxdx = (V.xc[i+1, j] - V.xc[i, j]) / dx 
-            dVydy = (V.yc[i, j+1] - V.yc[i, j]) / dy
-
-            P.c[i, j] = P₀.c[i, j] - γ * (dVxdx + dVydy)
-
-            dVxdy_dVydx = 0.5*((V.xv[i+1, j+1] - V.xv[i+1, j]) / dy + (V.yv[i+1, j+1] - V.yv[i, j+1]) / dx)
-            
-            η.c[i, j] = 0.5 * B.c[i, j] * (0.5 * dVxdx^2 + 0.5 * dVydy^2 + dVxdy_dVydx^2 + 2 * ϵ̇_bg^2) ^ (0.5q - 1)
-
-
-        end
-    end
-
-    for j = 1:ny+1
-        for i = 1:nx+1
-            dVxdx = (V.xv[i+1, j] - V.xv[i, j]) / dx
-            dVydy = (V.yv[i, j+1] - V.yv[i, j]) / dy
-
-            P.v[i, j] = P₀.v[i, j] - γ * (dVxdx + dVydy)
-
-            dVxdy = 1 < j < ny+1 ? 0.5 * (V.xc[i, j] - V.xc[i, j-1]) / dy : 0.  # gradient of wall parallel velocity is zero
-            dVydx = 1 < i < nx+1 ? 0.5 * (V.yc[i, j] - V.yc[i-1, j]) / dx : 0.  # gradient of wall parallel velocity is zero
-
-            η.v[i, j] = 0.5 * B.v[i, j] * (0.5 * dVxdx^2 + 0.5 * dVydy^2 + (dVxdy + dVydx)^2 + 2 * ϵ̇_bg^2) ^ (0.5q - 1)
-
-        end
-    end
-
-    ### residual in horizontal (x) directio
-    ## including Neumann BC on at top and bottom boundary
-    ## for velocities associated with cell centers (V.xc)
-    for j = 1:ny  # all values in y direction
-        for i = 2:nx  # inner values in x direction
-            # stress at horizontally adjacent cell centers
-            τxx_r = 2 * η.c[i  , j] * (V.xc[i+1, j] - V.xc[i  , j]) / dx
-            τxx_l = 2 * η.c[i-1, j] * (V.xc[i  , j] - V.xc[i-1, j]) / dx
-
-            # stress at vertically adjacent cell corners
-            if j > 1
-                τxy_b = η.v[i, j  ] * ((V.xc[i, j  ] - V.xc[i, j-1]) / dy + (V.yc[i, j  ] - V.yc[i-1, j  ]) / dx)
-            else
-                τxy_b = 0.  # zero stress at the bottom boundary
-            end
-
-            if j < ny
-                τxy_t = η.v[i, j+1] * ((V.xc[i, j+1] - V.xc[i, j  ]) / dy + (V.yc[i, j+1] - V.yc[i-1, j+1]) / dx)
-            else
-                τxy_t = 0.  # zero stress at the top boundary
-            end
-
-
-            # residual in x direction on the interface
-            R.xc[i, j]  = ( (τxx_r - τxx_l) / dx
-                          + (τxy_t - τxy_b) / dy
-                          - (P.c[i, j] - P.c[i-1, j]) / dx)
-        end
-    end
-    ## for velocities associated with cell corners (V.xv)
-    for j = 1:ny+1  # all values in y direction
-        for i = 2:nx+1  # inner values in x direction
-            τxx_r = 2 * η.v[i  , j] * (V.xv[i+1, j] - V.xv[i  , j]) / dx
-            τxx_l = 2 * η.v[i-1, j] * (V.xv[i  , j] - V.xv[i-1, j]) / dx
-
-            if j > 1
-                τxy_b = η.c[i-1, j-1] * ((V.xv[i, j  ] - V.xv[i, j-1]) / dy + (V.yv[i, j  ] - V.yv[i-1, j  ]) / dx)
-            else
-                τxy_b = 0.  # zero stress at the bottom boundary
-            end
-
-            if j < ny+1
-                τxy_t = η.c[i-1, j  ] * ((V.xv[i, j+1] - V.xv[i, j  ]) / dy + (V.yv[i, j+1] - V.yv[i-1, j+1]) / dx)
-            else
-                τxy_t = 0.  # zero stress at the top boundary
-            end
-
-            R.xv[i, j] = ( (τxx_r - τxx_l) / dx
-                         + (τxy_t - τxy_b) / dy
-                         - (P.v[i, j] - P.v[i-1, j]) / dx)
-        end
-    end
-
-    ### residual in vertical (y) direction
-    ### including Neumann BC at left and right boundary
-    ## for velocities associated with cell centers (V.yc)
-    for j = 2:ny  # inner values in y direction
-        for i = 1:nx  # all values in x direction
-            τyy_t = 2 * η.c[i, j  ] * (V.yc[i, j+1] - V.yc[i, j  ]) / dy
-            τyy_b = 2 * η.c[i, j-1] * (V.yc[i, j  ] - V.yc[i, j-1]) / dy
-
-            if i > 1
-                τxy_l = η.v[i  , j] * ((V.xc[i  , j] - V.xc[i  , j-1]) / dy + (V.yc[i  , j] - V.yc[i-1, j]) / dx)
-            else
-                τxy_l = 0.  # zero stress at the left boundary
-            end
-
-            if i < nx
-                τxy_r = η.v[i+1, j] * ((V.xc[i+1, j] - V.xc[i+1, j-1]) / dy + (V.yc[i+1, j] - V.yc[i  , j]) / dx)
-            else
-                τxy_r = 0.  # zero stress at the right boundary
-            end
-            
-            R.yc[i, j] = ( (τyy_t - τyy_b) / dy
-                         + (τxy_r - τxy_l) / dx
-                         - ( P.c[i, j] -  P.c[i, j-1]) / dy
-                         - (ρg.c[i, j] + ρg.c[i, j-1]) * 0.5)
-        end
-    end
-    ## for velocities associated with cell corners (V.yv)
-    for j=2:ny+1
-        for i=1:nx+1
-            τyy_t = 2 * η.v[i, j  ] * (V.yv[i, j+1] - V.yv[i, j  ]) / dy
-            τyy_b = 2 * η.v[i, j-1] * (V.yv[i, j  ] - V.yv[i, j-1]) / dy
-
-            if i > 1
-                τxy_l = η.c[i-1, j-1] * ((V.xv[i  , j] - V.xv[i  , j-1]) / dy + (V.yv[i  , j] - V.yv[i-1, j]) / dx)
-            else
-                τxy_l = 0.  # zero stress at the left boundary
-            end
-
-            if i < nx+1
-                τxy_r = η.c[i  , j-1] * ((V.xv[i+1, j] - V.xv[i+1, j-1]) / dy + (V.yv[i+1, j] - V.yv[i  , j]) / dx)
-            else
-                τxy_r = 0.  # zero stress at the right boundary
-            end
-
-            R.yv[i, j] = ( (τyy_t - τyy_b) / dy
-                         + (τxy_r - τxy_l) / dx
-                         - ( P.v[i, j] -  P.v[i, j-1]) / dy
-                         + (ρg.v[i, j] + ρg.v[i, j-1]) * 0.5)
-        end
-    end
-
-    # Residuals corresponding to cells affected by Dirichlet BC are left zero
-    return nothing
-end
-
-
-function update_D!(D, R, invM, β)
-    for j = 1:size(D.xc, 2)
-        for i = 2:size(D.xc, 1)-1
-            D.xc[i, j] = invM.xc[i, j] * R.xc[i, j] + β * D.xc[i, j]
-        end
-    end
-
-    for j = 2:size(D.yc, 2)-1
-        for i = 1:size(D.yc, 1)
-            D.yc[i, j] = invM.yc[i, j] * R.yc[i, j] + β * D.yc[i, j]
-        end
-    end
-
-    for j = 1:size(D.xv, 2)
-        for i = 2:size(D.xv, 1)-1
-            D.xv[i, j] = invM.xv[i, j] * R.xv[i, j] + β * D.xv[i, j]
-        end
-    end
-
-    for j = 2:size(D.yv, 2)-1
-        for i = 1:size(D.yv, 1)
-            D.yv[i, j] = invM.yv[i, j] * R.yv[i, j] + β * D.yv[i, j]
-        end
-    end
-    return nothing
-end
-
-
-function update_V!(V, D, α)
-    for j = 1:size(V.xc, 2)
-        for i = 2:size(V.xc, 1)-1
-            V.xc[i, j] += α * D.xc[i, j]
-        end
-    end
-    for j = 2:size(V.yc, 2)-1
-        for i = 1:size(V.yc, 1)
-            V.yc[i, j] += α * D.yc[i, j]
-        end
-    end
-
-    for j = 1:size(V.xv, 2)
-        for i = 2:size(V.xv, 1)-1
-            V.xv[i, j] += α * D.xv[i, j]
-        end
-    end
-
-    for j = 2:size(V.yv, 2)-1
-        for i = 1:size(V.yv, 1)
-            V.yv[i, j] += α * D.yv[i, j]
-        end
-    end
-    return nothing
-end
-
-
-function initialise_invM(invM, η, dx, dy, γ)
-    nx, ny = size(η.c)
-
-    ## inner points
-    # x direction, cell centers
-    for j = 2:ny-1
-        for i = 2:nx
-            mij = ( 2 / dx^2 * (η.c[i-1, j] + η.c[i, j  ])
-                  + 1 / dy^2 * (η.v[i  , j] + η.v[i, j+1])
-                  + 2 * γ / dx^2)
-            invM.xc[i, j] = inv(mij)
-        end
-    end
-
-    # y direction, cell centers
-    for j = 2:ny
-        for i = 2:nx-1
-            mij = ( 2 / dy^2 * (η.c[i, j-1] + η.c[i  , j])
-                  + 1 / dx^2 * (η.v[i, j  ] + η.v[i+1, j])
-                  + 2 * γ / dy^2)
-            invM.yc[i, j] = inv(mij)
-        end
-    end
-
-    # x direction, vertices
-    for j = 2:ny
-        for i = 2:nx+1
-            mij = ( 2 / dx^2 * (η.v[i-1, j  ] + η.v[i  , j])
-                  + 1 / dy^2 * (η.c[i-1, j-1] + η.c[i-1, j])
-                  + 2 * γ / dx^2)
-            invM.xv[i, j] = inv(mij)
-        end
-    end
-
-    # y direction, vertices
-    for j=2:ny+1
-        for i=2:nx
-            mij = ( 2 / dy^2 * (η.v[i  , j-1] + η.v[i, j  ])
-                  + 1 / dx^2 * (η.c[i-1, j-1] + η.c[i, j-1])
-                  + 2 * γ / dy^2)
-            invM.yv[i, j] = inv(mij)
-        end
-    end
-
-    ## Neumann boundary points
-    # x direction, cell centers
-    for i = 2:nx
-        invM.xc[i, 1 ] = inv( 2 / dx^2 * (η.c[i-1, 1] + η.c[i, 1])
-                            + 1 / dy^2 * (η.v[i, 2])
-                            + 2 * γ / dx^2)
-        invM.xc[i, ny] = inv( 2 / dx^2 * (η.c[i-1, ny] + η.c[i, ny])
-                            + 1 / dy^2 * η.v[i, ny]
-                            + 2 * γ / dx^2)
-    end
-    # y direction, cell centers
-    for j = 2:ny
-        invM.yc[1 , j] = inv( 2 / dy^2 * (η.c[1, j-1] + η.c[1, j])
-                            + 1 / dx^2 * (η.v[2, j])
-                            + 2 * γ / dy^2)
-        invM.yc[nx, j] = inv( 2 / dy^2  * (η.c[nx, j-1] + η.c[nx, j])
-                            + 1 / dx^2 * (η.v[nx, j])
-                            + 2 * γ / dy^2)
-    end
-    # x direction, vertices
-    for i = 2:nx+1
-        invM.xv[i, 1   ] = inv( 2 / dx^2 * (η.v[i-1, 1] + η.v[i, 1])
-                              + 1 / dy^2 * (η.c[i-1, 1])
-                              + 2 * γ / dx^2)
-        invM.xv[i, ny+1] = inv( 2 / dx^2 * (η.v[i-1, ny+1] + η.v[i, ny+1])
-                              + 1 / dy^2 * (η.c[i-1, ny  ])
-                              + 2 * γ / dx^2)
-    end
-    # y direction, vertices
-    for j = 2:ny+1
-        invM.yv[1   , j] = inv( 2 / dy^2 * (η.v[1, j-1] + η.v[1, j])
-                              + 1 / dx^2 * (η.c[1, j-1])
-                              + 2 * γ / dy^2)
-        invM.yv[nx+1, j] = inv( 2 / dy^2 * (η.v[nx+1, j-1] + η.v[nx+1, j])
-                              + 1 / dx^2 * (η.c[nx  , j-1])
-                              + 2 * γ / dy^2)
-    end
-
-    ## Dirichlet boundary points, leave zero
-
-    return nothing
-    
-end
-
-
-function nonlinear_inclusion(;n=127, ninc=5, η_ratio=0.1, niter=10000, γ_factor=1.,
+function nonlinear_inclusions(;n=127, ninc=5, η_ratio=0.1, niter=10000, γ_factor=1.,
                             ϵ_cg=1e-3, ϵ_ph=1e-6, ϵ_newton=1e-3, verbose=false)
     L_ref =  1. # reference length 
     ρg_avg = 1. # average density
@@ -473,7 +144,7 @@ function nonlinear_inclusion(;n=127, ninc=5, η_ratio=0.1, niter=10000, γ_facto
 
     ϵ̇_E  = zeros(nx, ny) # strain rate invariant, computed only for cell centers
     
-    initialise_η_ρ!(B, ρg, B_avg, ρg_avg, η_ratio, xc, yc, xv, yv, Lx, Ly, ninc=ninc)
+    initialise_B_ρ!(B, ρg, B_avg, ρg_avg, η_ratio, xc, yc, xv, yv, Lx, Ly, ninc=ninc)
 
     # Coefficient of augmented Lagrangian
     γ = γ_factor * tplNorm(B, Inf)
@@ -621,22 +292,5 @@ function nonlinear_inclusion(;n=127, ninc=5, η_ratio=0.1, niter=10000, γ_facto
 end
 
 
-function compute_strain_rate!(ϵ̇_E, V, dx, dy, ϵ̇_bg=eps())
-    nx, ny = size(ϵ̇_E)
-
-    for j = 1:ny
-        for i = 1:nx
-            dVxdx = (V.xc[i+1, j] - V.xc[i, j]) / dx 
-            dVydy = (V.yc[i, j+1] - V.yc[i, j]) / dy
-            dVxdy_dVydx = 0.5*((V.xv[i+1, j+1] - V.xv[i+1, j]) / dy + (V.yv[i+1, j+1] - V.yv[i, j+1]) / dx)
-            
-            ϵ̇_E[i, j] = (0.5 * dVxdx^2 + 0.5 * dVydy^2 + dVxdy_dVydx^2 + 2 * ϵ̇_bg^2)
-
-
-        end
-    end
-end
-
-
 n = 128
-outfields = nonlinear_inclusion(n=n, ninc=4, η_ratio=10.,γ_factor=100., niter=2000*n, ϵ_ph=1e-5, ϵ_cg=1e-5, ϵ_newton=1e-5);
+outfields = nonlinear_inclusions(n=n, ninc=4, η_ratio=10.,γ_factor=100., niter=2000*n, ϵ_ph=1e-5, ϵ_cg=1e-5, ϵ_newton=1e-5);
