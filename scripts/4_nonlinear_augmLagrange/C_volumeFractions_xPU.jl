f7a4879d63a2ee92f4e0a9da0dec7cd389dcfc50
chumbel <chumbel@ethz.ch>
2025-03-10 10:06:12 +0100

    first version of solver with volume fraction: Newton iteration diverges

---
+++ scripts/4_nonlinear_augmLagrange/C_volumeFractions_xPU.jl
@@ -0,0 +1,316 @@
+using CairoMakie
+using ColorSchemes
+using Enzyme
+using KernelAbstractions
+using Random
+using CUDA
+
+include("../../src/tuple_manip.jl")
+include("kernels_free_slip.jl")
+include("kernels_volume_fractions.jl")
+
+function nonlinear_inclusion(;n=126, niter=10000, γ_factor=1.,
+                            ϵ_cg=1e-3, ϵ_ph=1e-6, ϵ_newton=1e-3, freq_recompute=100,
+                            backend=CPU(), workgroup=64, type=Float64, verbose=false)
+    L_ref = 1. # reference length 
+    ρg_b  = 1. # background density
+
+    # physical parameters would be: n == 3, A = (24 * 1e-25) in Glen's law, see Cuffrey and Paterson (2006), table 3.3
+    # and q = 1. + 1/n, η_avg = (24 * 1e-25) ^ (-1/n), see Schoof (2006)
+    B_avg = 1. 
+    q = 1. + 1/3  
+
+    Lx = Ly = L_ref
+
+    rₐ = 1.6Lx
+    rₛ = 0.9Ly
+    x₀ = 0
+    y₀ = -1.2Ly
+    ri  = [0.1Lx, 0.15Lx, 0.2Lx]
+    xi  = [-0.3Lx, 0.0Lx, 0.2Ly]
+    yi  = [-0.3Ly, 0.2Ly, -0.2Ly]
+    ρgi = [0.0, 0.0, 0.0]
+    nx = ny = n
+    ϵ̇_bg = eps()
+
+    Δx,  Δy  = Lx / nx, Ly / ny
+    iΔx, iΔy = inv(Δx), inv(Δy)
+    xc = LinRange(-0.5Lx + 0.5Δx, 0.5Lx - 0.5Δx, nx)
+    yc = LinRange(-0.5Ly + 0.5Δy, 0.5Ly - 0.5Δy, ny)
+    xv = LinRange(-0.5Lx, 0.5Lx, nx+1)
+    yv = LinRange(-0.5Ly, 0.5Ly, ny+1)
+
+
+    # field initialisation
+    P    = (c=KernelAbstractions.zeros(backend, type, nx, ny),
+            v=KernelAbstractions.zeros(backend, type, nx+1, ny+1))
+    P₀   = deepcopy(P)  # old pressure
+    P̄    = deepcopy(P)  # memory needed for auto-differentiation
+    divV = deepcopy(P)  # velocity divergence
+    B    = deepcopy(P)  # prefactor of constituitive relation
+    # ϵ̇_E  = deepcopy(P)  # squared invariant of strain rate
+    τ    = (c=(xx=KernelAbstractions.zeros(backend, type, nx  , ny  ),
+               yy=KernelAbstractions.zeros(backend, type, nx  , ny  ),
+               xy=KernelAbstractions.zeros(backend, type, nx+2, ny+2)),
+            v=(xx=KernelAbstractions.zeros(backend, type, nx+1, ny+1),
+               yy=KernelAbstractions.zeros(backend, type, nx+1, ny+1),
+               xy=KernelAbstractions.zeros(backend, type, nx+1, ny+1)))  # deviatoric stress tensor
+    τ̄    = deepcopy(τ)
+    V    = (xc=KernelAbstractions.zeros(backend, type, nx+1, ny),
+            yc=KernelAbstractions.zeros(backend, type, nx, ny+1),
+            xv=KernelAbstractions.zeros(backend, type, nx+2, ny+1),
+            yv=KernelAbstractions.zeros(backend, type, nx+1, ny+2))
+    dV   = deepcopy(V)  # velocity updates in Newton iteration
+    V̄    = deepcopy(V)  # memory needed for auto-differentiation
+    D    = deepcopy(V)  # search direction of CG, cells affecting Dirichlet BC are zero
+    R    = deepcopy(V)  # nonlinear Residual
+    K    = deepcopy(V)  # Residuals in CG
+    Q    = deepcopy(V)  # Jacobian of compute_R wrt. V, multiplied by some vector (used for autodiff)
+    invM = deepcopy(V)  # preconditioner, cells correspoinding to Dirichlet BC are zero
+    f    = deepcopy(V)  # body force
+    ωₐ   = (c =KernelAbstractions.zeros(backend, type, nx+2, ny+2),
+            v =KernelAbstractions.zeros(backend, type, nx+1, ny+1),
+            xc=KernelAbstractions.zeros(backend, type, nx+1, ny),
+            yc=KernelAbstractions.zeros(backend, type, nx, ny+1),
+            xv=KernelAbstractions.zeros(backend, type, nx+2, ny+1),
+            yv=KernelAbstractions.zeros(backend, type, nx+1, ny+2))
+    ωₛ   = deepcopy(ωₐ)
+
+    R̂    = deepcopy(V)
+    P̂    = deepcopy(P)
+    τ̂    = deepcopy(τ)
+
+    initialise_volume_fractions_ring_segment!(ωₐ, ωₛ,  x₀, y₀, rₐ, rₛ, xc, yc, xv, yv)
+    initialise_f!(f, xi, yi, ri, ρgi, ρg_b, xc, yc, xv, yv)
+
+    tplFill!(B, B_avg)
+
+    γ = γ_factor
+
+
+    # residual norms for monitoring convergence
+    δ = Inf # CG
+    μ = Inf # CG
+    χ = Inf # Newton
+    ν = Inf # Pressure
+    
+    χ_ref = tplNorm(f)
+
+    # visualisation
+    itercounts = []
+    res_newton = []
+
+    fig = Figure(size=(800,900))
+    axs = (ρg=Axis(fig[1,1][1,1], aspect=1, title="ρg"),
+           ω =Axis(fig[1,2][1,1], aspect=1, title="volume fractions"),
+           Vx=Axis(fig[2,1][1,1], aspect=1, title="horizontal velocity"),
+           Vy=Axis(fig[2,2][1,1], aspect=1, title="vertical velocity"),
+           Pc=Axis(fig[3,1][1,1], aspect=1, title="pressure"),
+           Er=Axis(fig[3,2][1,1], title="Convergence of Newton Method", xlabel="iterations / nx", ylabel="residual norm")
+           )
+
+    plt = (ρg=heatmap!(axs.ρg, Array(f.yc), colormap=ColorSchemes.viridis),
+           ω =heatmap!(axs.ω , Array(ωₐ.c .* ωₛ.c), colormap=:greys),
+           Vx=heatmap!(axs.Vx, Array(V.xc), colormap=ColorSchemes.viridis),
+           Vy=heatmap!(axs.Vy, Array(V.yc), colormap=ColorSchemes.viridis),
+           Pc=heatmap!(axs.Pc, Array(P.c), colormap=ColorSchemes.viridis),
+           )
+
+    cbar= (ρg=Colorbar(fig[1, 1][1, 2], plt.ρg),
+           ω =Colorbar(fig[1, 2][1, 2], plt.ω),
+           Pc=Colorbar(fig[3, 1][1, 2], plt.Pc),
+           Vx=Colorbar(fig[2, 1][1, 2], plt.Vx),
+           Vy=Colorbar(fig[2, 2][1, 2], plt.Vy)
+           )
+
+    display(fig)
+
+    # create Kernels
+    up_D!      = update_D!(backend, workgroup, (nx+2, ny+2))
+    up_dV!     = update_V!(backend, workgroup, (nx+2, ny+2))
+    comp_divV! = compute_divV_weighted!(backend, workgroup, (nx+1, ny+1))
+    comp_P_τ!  = compute_P_τ_weighted!(backend, workgroup, (nx+1, ny+1))
+    comp_R!    = compute_R_weighted!(backend, workgroup, (nx+2, ny+2))
+    comp_K!    = compute_K!(backend, workgroup, (nx+2, ny+2))
+    up_K!      = update_K!(backend, workgroup, (nx+2, ny+2))
+    step_V!    = try_step_V!(backend, workgroup, (nx+2, ny+2))
+    init_D!    = initialise_D!(backend, workgroup, (nx+2, ny+2))
+    set!       = assign_flux_field!(backend, workgroup, (nx+2, ny+2))
+    set_one!   = set_part_to_ones!(backend, workgroup, (nx+2, ny+2))
+    set_part!  = assign_part!(backend, workgroup, (nx+2, ny+2))
+    inv!       = invert!(backend, workgroup, (nx+2, ny+2))
+
+    # create function for jacobian-vector product
+    
+    # compute Dv r * V̄ as Dp r * Dv p * V̄ + Dτ r * Dv τ * V̄
+    function jvp_R(R̂, Q, P, P̄, P̂, τ, τ̄, τ̂, V, V̄, P₀, f, B, q, ωₐ, ωₛ, ϵ̇_bg, iΔx, iΔy, γ)
+        tplSet!(P̂, P)
+        tplSet!(τ̂.c, τ.c)
+        tplSet!(τ̂.v, τ.v)
+        make_zero!(Q)
+        make_zero!(P̄)
+        make_zero!(τ̄)
+        autodiff(Forward, comp_P_τ!,
+                 DuplicatedNoNeed(P̂, P̄), DuplicatedNoNeed(τ̂, τ̄),
+                 Const(P₀),
+                 Duplicated(V, V̄),
+                 Const(B), Const(q), Const(ωₛ), Const(ϵ̇_bg), Const(iΔx), Const(iΔy), Const(γ))
+
+        autodiff(Forward, comp_R!,
+                 DuplicatedNoNeed(R̂, Q),
+                 Duplicated(P, P̄), Duplicated(τ, τ̄),
+                 Const(ωₐ), Const(ωₛ), Const(f), Const(iΔx), Const(iΔy))
+        return nothing
+    end
+
+    # function to compute the preconditioner
+    # overwrites invM, P̄, τ̄, V̄, Q, potentially recomputes R, P, τ
+    function initialise_invM!(invM, R̂, Q, P, P̄, P̂, τ, τ̄, τ̂, V, V̄, P₀, f, B, q, ϵ̇_bg, iΔx, iΔy, γ)
+        for (i, I) = enumerate(eachindex(invM))
+            set_one!(V̄, true, i)
+            jvp_R(R̂, Q, P, P̄, P̂, τ, τ̄, τ̂, V, V̄, P₀, f, B, q, ωₐ, ωₛ, ϵ̇_bg, iΔx, iΔy, γ)
+            set_part!(invM[I], Q[I], true)
+
+            set_one!(V̄, false, i)
+            jvp_R(R̂, Q, P, P̄, P̂, τ, τ̄, τ̂, V, V̄, P₀, f, B, q, ωₐ, ωₛ, ϵ̇_bg, iΔx, iΔy, γ)
+            set_part!(invM[I], Q[I], false)
+        end
+        inv!(invM)
+        return nothing
+    end
+
+
+    # Powell Hestenes
+    it = 0
+    while it < niter && ν > ϵ_ph
+        # p_0 = p
+        tplSet!(P₀, P)
+
+        # r = f - div τ + grad p - grad( div v)
+        comp_P_τ!(P, τ, P₀, V, B, q, ωₛ, ϵ̇_bg, iΔx, iΔy, γ)
+        comp_R!(R, P, τ, f, ωₐ, ωₛ, iΔx, iΔy)
+
+        χ = tplNorm(R) / χ_ref
+
+        # Newton iteration
+        while it < niter && χ > ϵ_newton
+            # initialise preconditioner
+            # inv(M) = inv(diag (Dv r))
+            initialise_invM!(invM, R̂, Q, P, P̄, P̂, τ, τ̄, τ̂, V, V̄, P₀, f, B, q, ϵ̇_bg, iΔx, iΔy, γ)
+            # reference for cg residual
+            μ_ref = tplDot(R, R, invM)
+        
+
+            # iteration zero
+            # compute residual for CG,
+            # k = r - Dv r * dv
+            set!(V̄, dV)
+            # use R̄ instead of R as first argument because it might get overwritten in autodiff
+            jvp_R(R̂, Q, P, P̄, P̂, τ, τ̄, τ̂, V, V̄, P₀, f, B, q, ωₐ, ωₛ, ϵ̇_bg, iΔx, iΔy, γ)
+            comp_K!(K, R, Q)
+
+            # d = inv(M) * k
+            init_D!(D, K, invM)
+            μ = tplDot(K, D)
+            # δ = tplNorm(K, Inf) / δ_ref
+            # start iteration
+            it_cg = 1
+            while it <= niter && μ > ϵ_cg^2 * μ_ref
+                # compute α
+                # α = k^T * inv(M) * k / (d^T * Dv r * d)
+                set!(V̄, D)
+                jvp_R(R̂, Q, P, P̄, P̂, τ, τ̄, τ̂, V, V̄, P₀, f, B, q, ωₐ, ωₛ, ϵ̇_bg, iΔx, iΔy, γ)
+                α = μ / tplDot(D, Q)
+
+                # dv += α d
+                up_dV!(dV, D, α)
+
+                # recompute residual
+                if it_cg % freq_recompute == 0
+                    # k = r - Dv r * dv
+                    set!(V̄, dV)
+                    jvp_R(R̂, Q, P, P̄, P̂, τ, τ̄, τ̂, V, V̄, P₀, f, B, q, ωₐ, ωₛ, ϵ̇_bg, iΔx, iΔy, γ)
+                    comp_K!(K, R, Q)
+                else
+                    # k = k - α Dv r * d
+                    up_K!(K, Q, α)
+                end
+
+
+                # μ = k^T inv(M) k
+                μ_new = tplDot(K, K, invM)
+                β = μ_new / μ
+                μ = μ_new
+                # d = β d + inv(M) * k 
+                up_D!(D, K, invM, β)
+
+                it_cg += 1
+                it += 1
+
+                if verbose && it_cg % n == 0
+                    println("CG residual = ", μ / μ_ref)
+                    plt.Pc[3][] .= Array(P.c)
+                    plt.Vx[3][] .= Array(K.xc)
+                    plt.Vy[3][] .= Array(K.yc)
+                    plt.Pc.colorrange[] = (min(-1e-10,minimum(P.c )), max(1e-10,maximum(P.c )))
+                    plt.Vx.colorrange[] = (min(-1e-10,minimum(K.xc)), max(1e-10,maximum(K.xc)))
+                    plt.Vy.colorrange[] = (min(-1e-10,minimum(K.yc)), max(1e-10,maximum(K.yc)))
+                    display(fig)
+                end
+
+                # periodically check for stagnation
+                if it_cg % freq_recompute == 0
+                    δ = α * tplNorm(D) / tplNorm(dV) 
+                    if δ < ϵ_cg
+                        println("GC relative update: ", δ)
+                        break
+                    end
+                end
+            end
+            # damped to newton iteration
+            # find λ st. r(v - λ dv) < r(v)
+            λ = 1.
+            step_V!(V̄, V, dV, λ)
+            comp_P_τ!(P, τ, P₀, V, B, q, ωₛ, ϵ̇_bg, iΔx, iΔy, γ)
+            comp_R!(R, P, τ, f, ωₐ, ωₛ, iΔx, iΔy)
+            χ_new = tplNorm(R) / χ_ref
+            while χ_new >= χ && λ > 1e-3
+                λ /= MathConstants.golden
+                step_V!(V̄, V, dV, λ)
+                comp_P_τ!(P, τ, P₀, V, B, q, ωₛ, ϵ̇_bg, iΔx, iΔy, γ)
+                comp_R!(R, P, τ, f, ωₐ, ωₛ, iΔx, iΔy)
+                χ_new = tplNorm(R) / χ_ref
+            end
+            tplSet!(V, V̄)
+            χ = χ_new
+ 
+            push!(itercounts, it)
+            push!(res_newton, χ)
+
+            # update plot
+            plt.Pc[3][] .= Array(P.c)
+            plt.Vx[3][] .= Array(V.xc)
+            plt.Vy[3][] .= Array(V.yc)
+            # plt.Sr[3][] .= log10.(Array(ϵ̇_E.c))
+            plt.Pc.colorrange[] = (min(-1e-10,minimum(P.c )), max(1e-10,maximum(P.c )))
+            plt.Vx.colorrange[] = (min(-1e-10,minimum(V.xc)), max(1e-10,maximum(V.xc)))
+            plt.Vy.colorrange[] = (min(-1e-10,minimum(V.yc)), max(1e-10,maximum(V.yc)))
+            # plt.Sr.colorrange[] = (min(-1,log10(minimum(ϵ̇_E.c))), max(1,log10(maximum(ϵ̇_E.c))))
+
+            scatterlines!(axs.Er, itercounts ./ nx, log10.(res_newton), color=:purple)
+
+            display(fig)
+            
+            println("Newton residual = ", χ, "; λ = ", λ, "; total iteration count: ", it)
+        end    
+        comp_divV!(divV, V, ωₛ, iΔx, iΔy)
+        ν = tplNorm(divV) / tplNorm(P)
+        println("Pressure residual = ", ν, ", Newton residual = ", χ, ", CG residual = ", δ)
+     end
+ 
+     return it, P, V, R
+end
+
+n = 62
+nonlinear_inclusion(n=n, γ_factor=100., niter=1000, ϵ_ph=1e-5, ϵ_cg=1e-5, ϵ_newton=1e-5, freq_recompute=100, verbose=true);
+
---
+++ scripts/4_nonlinear_augmLagrange/kernels_volume_fractions.jl
@@ -0,0 +1,199 @@
+# dimensions for kernel launch: nx+1, ny+1
+@kernel inbounds=true function compute_P_τ_weighted!(P, τ, P₀, V, B, q, ωₛ, ϵ̇_bg, iΔx, iΔy, γ)
+    i, j = @index(Global, NTuple)
+    if i <= size(P.c, 1) && j <= size(P.c, 2)
+        # wall BC
+        if i == 1
+            V.xc[i, j] = 0.
+            τ.c.xy[i, j+1] = 0.
+        end
+
+        if i + 1 == size(V.xc, 1)
+            V.xc[i+1, j] = 0.
+            τ.c.xy[i+2, j+1] = 0.
+        end
+
+        if j == 1
+            V.yc[i, j] = 0.
+            τ.c.xy[i+1, j] = 0.
+        end
+
+        if j + 1 == size(V.yc, 2)
+            V.yc[i, j+1] = 0.
+            τ.c.xy[i+1, j+2] = 0.
+        end
+
+        # Pressure update
+        dVxdx = (V.xc[i+1, j] * ωₛ.xc[i+1, j] - V.xc[i, j] * ωₛ.xc[i, j]) * iΔx 
+        dVydy = (V.yc[i, j+1] * ωₛ.yc[i, j+1] - V.yc[i, j] * ωₛ.yc[i, j]) * iΔy
+
+        P.c[i, j] = P₀.c[i, j] - γ * (dVxdx + dVydy)
+        
+        # Stress update
+        dVxdy_dVydx = 0.5 * ( (V.xv[i+1, j+1] * ωₛ.xv[i+1, j+1] - V.xv[i+1, j] * ωₛ.xv[i+1, j]) * iΔy
+                            + (V.yv[i+1, j+1] * ωₛ.yv[i+1, j+1] - V.yv[i, j+1] * ωₛ.yv[i, j+1]) * iΔx)
+
+        η = 0.5 * B.c[i, j] * (0.5 * dVxdx^2 + 0.5 * dVydy^2 + dVxdy_dVydx^2 + 2 * ϵ̇_bg^2)^(0.5q - 1)
+        τ.c.xx[i, j] = 2 * η * dVxdx
+        τ.c.yy[i, j] = 2 * η * dVydy
+        τ.c.xy[i+1, j+1] = 2 * η * dVxdy_dVydx
+    end
+
+    if i <= size(P.v, 1) && j <= size(P.v, 2)
+        if i == 1
+            V.xv[i, j] = 0.
+        end
+
+        if i + 1 == size(V.xv, 1)
+            V.xv[i+1, j] = 0.
+        end
+
+        if j == 1
+            V.yv[i, j] = 0.
+        end
+
+        if j + 1 == size(V.yv, 2)
+            V.yv[i, j+1] = 0.
+        end
+        
+        dVxdx = (V.xv[i+1, j] * ωₛ.xv[i+1, j] - V.xv[i, j] * ωₛ.xv[i, j]) * iΔx
+        dVydy = (V.yv[i, j+1] * ωₛ.yv[i, j+1] - V.yv[i, j] * ωₛ.yv[i, j]) * iΔy
+
+        P.v[i, j] = P₀.v[i, j] - γ * (dVxdx + dVydy)
+
+        if 1 < i < size(P.v, 1) && 1 < j < size(P.v, 2)
+            dVxdy_dVydx = 0.5 * ( (V.xc[i, j] * ωₛ.xc[i, j] - V.xc[i, j-1] * ωₛ.xc[i, j-1]) * iΔy 
+                                + (V.yc[i, j] * ωₛ.yc[i, j] - V.yc[i-1, j] * ωₛ.yc[i-1, j]) * iΔx)
+        else
+            dVxdy_dVydx = 0.
+        end
+
+        η = 0.5 * B.v[i, j] * ((0.5 * dVxdx^2 + 0.5 * dVydy^2 + dVxdy_dVydx^2 + 2 * ϵ̇_bg^2) ^ (0.5q - 1))
+        τ.v.xx[i, j] = 2 * η * dVxdx
+        τ.v.yy[i, j] = 2 * η * dVydy
+        τ.v.xy[i, j] = 2 * η * dVxdy_dVydx
+    end
+end
+
+
+# dimensions for kernel launch: nx+2, ny+2
+@kernel inbounds=true function compute_R_weighted!(R, P, τ, f, ωₐ, ωₛ, iΔx, iΔy)
+    i, j = @index(Global, NTuple)
+
+    ### residual in horizontal (x) direction
+    ## including Neumann BC on at top and bottom boundary
+    ## for velocities associated with cell centers (V.xc)
+    if 1 < i < size(R.xc, 1) && j <= size(R.xc, 2)
+        # all values in y direction
+        # inner values in x direction
+
+        # residual in x direction on the interface
+        R.xc[i, j] = -((τ.c.xx[i, j  ] * ωₐ.c[i+1, j+1] - τ.c.xx[i-1, j] * ωₐ.c[i, j+1]) * iΔx
+                     + (τ.v.xy[i, j+1] * ωₐ.v[i  , j+1] - τ.v.xy[i  , j] * ωₐ.v[i, j  ]) * iΔy
+                     - (P.c[i, j] * ωₐ.c[i+1, j+1] - P.c[i-1, j]* ωₐ.c[i, j+1]) * iΔx
+                     - f.xc[i, j] * ωₐ.xc[i, j]) * ωₛ.xc[i, j]
+    end
+    ## for velocities associated with cell corners (V.xv)
+    if 1 < i < size(R.xv, 1) && j <= size(R.xv, 2)
+        # all values in y direction
+        # inner values in x direction
+        R.xv[i, j] = -((τ.v.xx[i, j  ] * ωₐ.v[i, j  ] - τ.v.xx[i-1, j] * ωₐ.v[i-1, j]) * iΔx
+                     + (τ.c.xy[i, j+1] * ωₐ.c[i, j+1] - τ.c.xy[i  , j] * ωₐ.c[i  , j]) * iΔy
+                     - (P.v[i, j] * ωₐ.v[i, j] - P.v[i-1, j] * ωₐ.v[i-1, j]) * iΔx
+                     - f.xv[i, j] * ωₐ.xv[i, j]) * ωₛ.xv[i, j]
+    end
+
+    ### residual in vertical (y) direction
+    ### including Neumann BC at left and right boundary
+    ## for velocities associated with cell centers (V.yc)
+    if i <= size(R.yc, 1) && 1 < j < size(R.yc, 2)
+        # inner values in y direction
+        # all values in x direction        
+        R.yc[i, j] = -((τ.c.yy[i  , j] * ωₐ.c[i+1, j+1] - τ.c.yy[i, j-1] * ωₐ.c[i+1, j]) * iΔy
+                     + (τ.v.xy[i+1, j] * ωₐ.v[i+1, j  ] - τ.v.xy[i, j  ] * ωₐ.v[i  , j]) * iΔx
+                     - (P.c[i, j] * ωₐ.c[i+1, j+1] -  P.c[i, j-1] * ωₐ.c[i+1, j]) * iΔy
+                     - f.yc[i, j] * ωₐ.yc[i, j]) * ωₛ.yc[i, j]
+    end
+    ## for velocities associated with cell corners (V.yv)
+    if i <= size(R.yv, 1) && 1 < j < size(R.yv, 2)
+        R.yv[i, j] = -((τ.v.yy[i  , j] * ωₐ.v[i  , j] - τ.v.yy[i, j-1] * ωₐ.v[i, j-1]) * iΔy
+                     + (τ.c.xy[i+1, j] * ωₐ.c[i+1, j] - τ.c.xy[i, j  ] * ωₐ.c[i, j  ]) * iΔx
+                     - (P.v[i, j] * ωₐ.v[i, j] -  P.v[i, j-1] * ωₐ.v[i, j-1]) * iΔy
+                     - f.yv[i, j] * ωₐ.yv[i, j]) * ωₛ.yv[i, j]
+    end
+
+    # Residuals corresponding to cells affected by Dirichlet BC are left zero
+end
+
+
+@kernel inbounds=true function compute_divV_weighted!(divV, V, ωₛ, iΔx, iΔy)
+    i, j = @index(Global, NTuple)
+    
+    if i < size(V.xc, 1) && j < size(V.yc, 2)
+        dVxdx = (V.xc[i+1, j] * ωₛ.xc[i+1, j] - V.xc[i, j] * ωₛ.xc[i, j]) * iΔx 
+        dVydy = (V.yc[i, j+1] * ωₛ.yc[i, j+1] - V.yc[i, j] * ωₛ.yc[i, j]) * iΔy
+        divV.c[i, j] = dVxdx + dVydy
+    end
+
+    if i < size(V.xv, 1) && j < size(V.yv, 2)
+        dVxdx = (V.xv[i+1, j] * ωₛ.xv[i+1, j] - V.xv[i, j] * ωₛ.xv[i, j]) * iΔx
+        dVydy = (V.yv[i, j+1] * ωₛ.yv[i, j+1] - V.yv[i, j] * ωₛ.yv[i, j]) * iΔy
+        divV.v[i, j] = dVxdx + dVydy
+    end
+end
+
+
+@views function initialise_volume_fractions_ring_segment!(ωₐ, ωₛ,  x₀, y₀, rₐ, rₛ, xc, yc, xv, yv)
+    copyto!(ωₐ.c[2:end-1, 2:end-1], [(x - x₀)^2 + (y - y₀)^2 < rₐ^2 ? 1. : 0. for x=xc, y=yc])
+    ωₐ.c[[1, end], :] .= ωₐ.c[[2, end-1], :]
+    ωₐ.c[:, [1, end]] .= ωₐ.c[:, [2, end-1]]
+    copyto!(ωₐ.v , [(x - x₀)^2 + (y - y₀)^2 < rₐ^2 ? 1. : 0. for x=xv, y=yv])
+    copyto!(ωₐ.xc, [(x - x₀)^2 + (y - y₀)^2 < rₐ^2 ? 1. : 0. for x=xv, y=yc])
+    copyto!(ωₐ.yc, [(x - x₀)^2 + (y - y₀)^2 < rₐ^2 ? 1. : 0. for x=xc, y=yv])
+    copyto!(ωₐ.xv[2:end-1, :], [(x - x₀)^2 + (y - y₀)^2 < rₐ^2 ? 1. : 0. for x=xc, y=yv])
+    copyto!(ωₐ.yv[:, 2:end-1], [(x - x₀)^2 + (y - y₀)^2 < rₐ^2 ? 1. : 0. for x=xv, y=yc])
+    ωₐ.xv[[1, end], :] .= ωₐ.xv[[2, end-1], :]
+    ωₐ.yv[:, [1, end]] .= ωₐ.yv[:, [2, end-1]]
+
+    copyto!(ωₛ.c[2:end-1, 2:end-1], [(x - x₀)^2 + (y - y₀)^2 < rₛ^2 ? 0. : 1. for x=xc, y=yc])
+    ωₛ.c[[1, end], :] .= ωₛ.c[[2, end-1], :]
+    ωₛ.c[:, [1, end]] .= ωₛ.c[:, [2, end-1]]
+    copyto!(ωₛ.v,  [(x - x₀)^2 + (y - y₀)^2 < rₛ^2 ? 0. : 1. for x=xv, y=yv])
+    copyto!(ωₛ.xc, [(x - x₀)^2 + (y - y₀)^2 < rₛ^2 ? 0. : 1. for x=xv, y=yc])
+    copyto!(ωₛ.yc, [(x - x₀)^2 + (y - y₀)^2 < rₛ^2 ? 0. : 1. for x=xc, y=yv])
+    copyto!(ωₛ.xv[2:end-1, :], [(x - x₀)^2 + (y - y₀)^2 < rₛ^2 ? 0. : 1. for x=xc, y=yv])
+    copyto!(ωₛ.yv[:, 2:end-1], [(x - x₀)^2 + (y - y₀)^2 < rₛ^2 ? 0. : 1. for x=xv, y=yc])
+    ωₛ.xv[[1, end], :] .= ωₛ.xv[[2, end-1], :]
+    ωₛ.yv[:, [1, end]] .= ωₛ.yv[:, [2, end-1]]
+    return nothing
+end
+
+@views function initialise_f!(f, xi, yi, ri, ρgi, ρg_b, xc, yc, xv, yv)
+    fill!(f.xc, 0)
+    fill!(f.xv, 0)  
+
+    fyc = Array(f.yc)
+    fyv = Array(f.yv)
+    fill!(fyc, ρg_b)  
+    fill!(fyv, ρg_b)  
+    for (_xi, _yi, _ri, _ρgi) = zip(xi, yi, ri, ρgi)
+        for j = eachindex(yv)
+            for i = eachindex(xc)
+                if (xc[i] - _xi)^2 + (yv[j] - _yi)^2 < _ri^2
+                    fyc[i, j] = _ρgi
+
+                end
+            end
+        end
+        for j = eachindex(yc)
+            for i = eachindex(xv)
+                if (xv[i] - _xi)^2 + (yc[j] - _yi)^2 < _ri^2
+                    fyv[i, j] = _ρgi
+                end
+            end
+        end
+    end
+    copyto!(f.yc, fyc)
+    copyto!(f.yv, fyv)
+    return nothing
+end
