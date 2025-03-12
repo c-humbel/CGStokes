a8d11d20c09f8a767cf0b6c44eeec05e4d67fc8d
chumbel <chumbel@ethz.ch>
2025-03-12 16:58:39 +0100

    run configuration for superzack

--- scripts/5_experiments/run.jl
+++ scripts/5_experiments/run.jl
@@ -1,15 +1,17 @@
 include("Ismip_E000_Arolla.jl")
 
-args = Dict(:n              => 10,
+args = Dict(:n              => 1000,
             :aspect         => 0.3,
             :γ_factor       => 1e5,
-            :niter          => 5000n,
+            :niter          => 5000000,
             :ϵ_ph           => 1e-8,
             :ϵ_cg           => 1e-10,
-            :ϵ_newton       => 1e-9,
+            :ϵ_newton       => 1e-8,
             :freq_recompute => 50,
-            :verbose        =>false)
+            :verbose        => true,
+            :backend        => CUDABackend(),
+            :workgroup      => (32, 8))
 
 out = run("data/arolla100.dat"; args...)
 dat = extract_data(out...)
-create_summary_plots(dat...; savefig=false)
+create_summary_plots(dat...; savefig=true)
